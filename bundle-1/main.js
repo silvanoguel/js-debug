6777ebd3fcab3a5329114d1f7bdc5d7c27bb6dbf
silvanoguel <silvanoguel@msn.com>
2023-03-27 16:49:35 +0200

    add:es1 completed

---
+++ bundle-1/main.js
@@ -0,0 +1,71 @@
+/*******************************************************************************
+
+    Per ognuno dei seguenti esercizi, rispondi a queste domande:
+    1- Che cosa fa questo codice?
+    2- Sono presenti errori di sintassi?
+    3- Sono presenti errori logici?
+
+    Inserisci i tuoi commenti con le risposte, sotto ciascun esercizio
+
+*******************************************************************************/
+
+
+// ESERCIZIO 1
+function checkAge() {
+    let myAge = 32 ;
+    let message = '';
+
+    if (myAge < 18) {
+        message = `Sei troppo giovane! Hai ${myAge} anni!`;
+    } else {
+        message = 'Hai più di 18 anni!';
+    }
+
+}
+checkAge();
+
+// 1-mettendo un età mi uscirà il messaggio se sono giovane o se ho più di 18 anni
+// 2-manca il return
+
+
+// // ESERCIZIO 2
+// function printColorsNumber() {
+//     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
+//     console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
+// }
+// printColorsNumber();
+
+
+// // ESERCIZIO 3
+// function addNumbers() {
+//     const userNumber = prompt('Inserisci un numero');
+//     const total = userNumber + 12;
+
+//     console.log(`Il risultato finale è ${total}`);
+// }
+// addNumbers();
+
+
+// // ESERCIZIO 4
+// function checkAccess() {
+//     const addresses = [
+//         'mymail@mail.com',
+//         'yourmail@mail.com',
+//         'hermail@mail.com',
+//         'hismail@mail.com',
+//     ];
+//     const userEmail = prompt('Inserisci il tuo indirizzo email');
+
+//     let grantAccess = 'false';
+
+//     if (addresses.includes(userEmail)) {
+//         grantAccess = 'true';
+//     }
+
+//     if (grantAccess === true) {
+//         console.log('Accesso consentito!');
+//     } else {
+//         console.log('Accesso negato!');
+//     }
+// }
+// checkAccess();
